923cd4b6807fe9d1bc74940ac195ab21a97da88f
francisohara24 <franciskohara@gmail.com>
2023-08-12 18:21:31 +0000

    Feat: update data directory
    update directory containing CVs

--- cv_emailer/cv_emailer.py
+++ cv_emailer/cv_emailer.py
@@ -6,8 +6,7 @@ from os import listdir
 # instantiate SMTP client
 client = sender.create_client()
 
-for cv in listdir("../data"):
-    path_to_cv = "../data/" + cv
+for cv in listdir("../data/current"):
+    path_to_cv = "../data/current/" + cv
     recipient = extractor.extract_address(path_to_cv)
     sender.send_cv(recipient, path_to_cv, client)
-
